45e040cdc2fee3ac4ceabdc0f331a2c0103c939a
Kamil Grabek <kamil.grabek@allegro.pl>
2018-12-08 17:42:54 +0100

    dodanie sortowania

--- client/src/App.js
+++ client/src/App.js
@@ -6,21 +6,45 @@ import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 
 const SERVER_HOST = 'http://localhost:3000/';
 
 class App extends Component {
-  state = null;
+  state = {
+    data: null,
+    sortBy: '',
+  };
 
   componentDidMount() {
-    fetch(SERVER_HOST)
+    const params = new URLSearchParams(window.location.search);
+    const sortBy = params.get('sortBy');
+
+    this.fetchList(sortBy);
+  }
+
+  fetchList(sortBy) {
+    return fetch(`${SERVER_HOST}?sortBy=${sortBy}`)
       .then(response => response.json())
-      .then(data => this.setState(data))
+      .then(data => this.setState({ data, sortBy }))
       .catch(error => console.error(error))
   }
 
+  handleSortByChange = event => {
+    const sortBy = event.target.value;
+
+    this.fetchList(sortBy).then(() => {
+      window.history.pushState(null, null, `?sortBy=${sortBy}`);
+    });
+  };
+
   render() {
-    if (!this.state || !this.state.list) {
+    if (!this.state.data || !this.state.data.list) {
       return (
         <CircularProgress />
       );
@@ -28,8 +52,29 @@ class App extends Component {
 
     return (
       <div>
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <form autoComplete="off">
+              <FormControl style={{ width: 150 }}>
+                <InputLabel htmlFor="sortBy">Sortowanie</InputLabel>
+                <Select
+                  value={this.state.sortBy || ''}
+                  onChange={this.handleSortByChange}
+                  inputProps={{
+                    name: 'sortBy',
+                    id: 'sortBy',
+                  }}
+                >
+                  <MenuItem value="title">Tytuł</MenuItem>
+                  <MenuItem value="publisher">Wydawca</MenuItem>
+                  <MenuItem value="category">Kategoria</MenuItem>
+                </Select>
+              </FormControl>
+            </form>
+          </Toolbar>
+        </AppBar>
         <Grid style={{ padding: 24 }} container spacing={24}>
-        {this.state.list.map((game, index) => (
+        {this.state.data.list.map((game, index) => (
           <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
             <Card>
               <CardActionArea>
--- index.js
+++ index.js
@@ -5,8 +5,27 @@ const data = require('./data');
 const app = express();
 
 app.get('/', cors(), function (req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(data));
+  const sortBy = req.query.sortBy;
+  const sortedData = {
+    list: data.list.sort((a, b) => {
+      const aValue = a[sortBy];
+      const bValue = b[sortBy];
+
+      if (aValue && bValue) {
+        if (aValue < bValue) {
+          return -1;
+        }
+        if (aValue > bValue) {
+          return 1;
+        }
+      }
+
+      return 0;
+    })
+  };
+
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(sortedData));
 });
 
 app.listen(3000);
