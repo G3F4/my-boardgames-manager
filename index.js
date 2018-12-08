37f3fc53b3720b28dafe2a56881749bbadab6004
Kamil Grabek <kamil.grabek@allegro.pl>
2018-12-08 13:47:22 +0100

    dodanie serwera express z mockoowymi danymi + skrypty start i dev

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const data = require('./data');
+
+const app = express();
+
+app.get('/', function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(data));
+});
+
+app.listen(3000);
